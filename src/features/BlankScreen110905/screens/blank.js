c40b5a73e68c24acd59d33333900e8c5413f64a5
Crowdbotics <team@crowdbotics.com>
2020-10-09 17:39:16 +0000

    Update components

--- src/features/BlankScreen110905/screens/blank.js
+++ src/features/BlankScreen110905/screens/blank.js
@@ -12,149 +12,31 @@ import {
   Datepicker
 } from "react-native-ui-kitten"
 import Slider from "@react-native-community/slider"
-
 import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 import { connect } from "react-redux"
-export class __Blank extends React.Component {
+export class _Blank extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
   }
   state = {}
   render = () => (
-    <View style={this.props.themedStyle.View_1}>
-      <View style={this.props.themedStyle.View_5}>
-        <View style={this.props.themedStyle.View_6} />
-        <View style={this.props.themedStyle.View_7} />
+    <View style={styles.View_1}>
+      <View style={styles.View_5}>
+        <View style={styles.View_6}>
+          <Text style={styles.Text_9}>Sample text content</Text>
+        </View>
+        <View style={styles.View_7} />
       </View>
     </View>
   )
 }
 
-_Blank = withStyles(__Blank, theme => ({
-  View_1: {
-    marginLeft: 0,
-    marginRight: 0,
-    marginTop: 0,
-    marginBottom: 0,
-    paddingLeft: 0,
-    paddingRight: 0,
-    paddingTop: 0,
-    paddingBottom: 0,
-    overflow: "visible",
-    fontSize: 12,
-    color: "#000000",
-    backgroundColor: "#ffffff",
-    fontStyle: "normal",
-    fontWeight: "normal",
-    flexDirection: "column",
-    flexWrap: "wrap",
-    justifyContent: "flex-start",
-    alignItems: "stretch",
-    alignContent: "stretch",
-    borderColor: "#000000",
-    borderStyle: "solid",
-    borderWidth: 0,
-    borderLeftWidth: 0,
-    borderRightWidth: 0,
-    borderTopWidth: 0,
-    borderBottomWidth: 0,
-    borderRadius: 0,
-    backgroundSize: "auto"
-  },
-  View_5: {
-    marginLeft: 0,
-    marginRight: 0,
-    marginTop: 0,
-    marginBottom: 0,
-    paddingLeft: 0,
-    paddingRight: 0,
-    paddingTop: 0,
-    paddingBottom: 0,
-    overflow: "visible",
-    fontSize: 12,
-    color: "#000000",
-    backgroundColor: "#ffffff",
-    fontStyle: "normal",
-    fontWeight: "normal",
-    flexDirection: "column",
-    flexWrap: "wrap",
-    justifyContent: "flex-start",
-    alignItems: "stretch",
-    alignContent: "stretch",
-    borderColor: "#000000",
-    borderStyle: "solid",
-    borderWidth: 0,
-    borderLeftWidth: 0,
-    borderRightWidth: 0,
-    borderTopWidth: 0,
-    borderBottomWidth: 0,
-    borderRadius: 0,
-    backgroundSize: "auto"
-  },
-  View_6: {
-    marginLeft: 0,
-    marginRight: 0,
-    marginTop: 0,
-    marginBottom: 0,
-    paddingLeft: 0,
-    paddingRight: 0,
-    paddingTop: 0,
-    paddingBottom: 0,
-    overflow: "visible",
-    fontSize: 12,
-    color: "#000000",
-    backgroundColor: "#ffffff",
-    fontStyle: "normal",
-    fontWeight: "normal",
-    flexDirection: "column",
-    flexWrap: "wrap",
-    justifyContent: "flex-start",
-    alignItems: "stretch",
-    alignContent: "stretch",
-    borderColor: "#000000",
-    borderStyle: "solid",
-    borderWidth: 0,
-    borderLeftWidth: 0,
-    borderRightWidth: 0,
-    borderTopWidth: 0,
-    borderBottomWidth: 0,
-    borderRadius: 0,
-    backgroundSize: "auto"
-  },
-  View_7: {
-    marginLeft: 0,
-    marginRight: 0,
-    marginTop: 0,
-    marginBottom: 0,
-    paddingLeft: 0,
-    paddingRight: 0,
-    paddingTop: 0,
-    paddingBottom: 0,
-    overflow: "visible",
-    fontSize: 12,
-    color: "#000000",
-    backgroundColor: "#ffffff",
-    fontStyle: "normal",
-    fontWeight: "normal",
-    flexDirection: "column",
-    flexWrap: "wrap",
-    justifyContent: "flex-start",
-    alignItems: "stretch",
-    alignContent: "stretch",
-    borderColor: "#000000",
-    borderStyle: "solid",
-    borderWidth: 0,
-    borderLeftWidth: 0,
-    borderRightWidth: 0,
-    borderTopWidth: 0,
-    borderBottomWidth: 0,
-    borderRadius: 0,
-    backgroundSize: "auto"
-  }
+Blank = withStyles(_Blank, theme => ({
+  container: { backgroundColor: theme["color-basic-100"] }
 }))
 
 function mapStateToProps(state) {
-  return { state: state }
+  return { state: state.apiReducer }
 }
 const actionCreators = {}
-export default connect(mapStateToProps, actionCreators)(_Blank)
+export default connect(mapStateToProps, actionCreators)(Blank)
